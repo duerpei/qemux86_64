bbc12e072a835184c4097e1b5e6d0232f2e0e0cc
duerpei <duep.fnst@fujitsu.com>
2023-02-22 10:26:57 +0800

    find files

--- debug.sh
+++ debug.sh
@@ -1,8 +1,9 @@
 ls /var/run/agl-test/logs/
 
-tree /var/run/agl-test/logs/
-
 ls /var/run/agl-test/logs/log-to-report/
 
 ls /var/run/agl-test/logs/tmp-log/
 
+ls /var/run/agl-test/logs/tmp-log/stress-ng/
+
+ls /var/run/agl-test/logs/tmp-log/test-report/
